8f73d57753e970d3b0682ceac1328a1c5428803e
totto2727 <kaihatu.totto2727@gmail.com>
2024-07-13 02:31:00 +0900

    feat: auto image deploy step

--- main.ts
+++ main.ts
@@ -11,16 +11,23 @@ type GhcrDockerTagsResponse = { name: string }[];
 const config = {
     GITHUB_TOKEN: Deno.env.get("GITHUB_TOKEN"),
     GITHUB_OUTPUT: Deno.env.get("GITHUB_OUTPUT"),
+    ARCHITECTURE: Deno.env.get("ARCHITECTURE"),
     org: "totto2727-org",
     packageType: "container",
     packageName: "playwright-ja",
 };
 
-if (!config.GITHUB_TOKEN || !config.GITHUB_OUTPUT) {
-    throw new Error("GITHUB_TOKEN, and GITHUB_OUTPUT are not set");
+if (!config.GITHUB_TOKEN || !config.GITHUB_OUTPUT || !config.ARCHITECTURE) {
+    throw new Error("GITHUB_TOKEN, GITHUB_OUTPUT, and ARCHITECTURE are not set");
 }
 
+const GITHUB_TOKEN = config.GITHUB_TOKEN;
+const GITHUB_OUTPUT = config.GITHUB_OUTPUT;
+const ARCHITECTURE = config.ARCHITECTURE;
+
+
 async function main() {
+
     const responseSource = await fetch(
         "https://mcr.microsoft.com/v2/playwright/tags/list",
     );
@@ -33,6 +40,7 @@ async function main() {
         filter((v) => v.startsWith("v")),
         filter((v) => canParse(v) && greaterThan(parse(v), parse("1.40.0"))),
         filter((v) => !v.includes("-alpha") && !v.includes("-beta")),
+        filter((v) => v.includes(ARCHITECTURE)),
     );
 
     const responseTarget = await fetch(
@@ -41,7 +49,7 @@ async function main() {
             method: "GET",
             headers: {
                 "Accept": "application/vnd.github+json",
-                "Authorization": `Bearer ${config.GITHUB_TOKEN}`,
+                "Authorization": `Bearer ${GITHUB_TOKEN}`,
                 "X-GitHub-Api-Version": "2022-11-28",
             },
         },
@@ -55,7 +63,7 @@ async function main() {
     const requiredBuildTagList = { tag: filteredVersion.filter((v) => !setTarget.has(v)) };
 
     Deno.writeTextFileSync(
-        config.GITHUB_OUTPUT!,
+        GITHUB_OUTPUT,
         `matrix=${JSON.stringify(requiredBuildTagList)}\n`,
         { append: true },
     );
