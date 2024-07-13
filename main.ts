71e10b22a170688a3aa09af2868938a09c71bfb5
totto2727 <kaihatu.totto2727@gmail.com>
2024-07-13 23:05:04 +0900

    feat: support multi platform build

--- main.ts
+++ main.ts
@@ -1,77 +1,145 @@
-import { filter, map, pipe, tap } from "npm:remeda";
-import { canParse, greaterThan, parse } from "jsr:@std/semver";
+import { filter, last, map, pipe, reverse, sort, splitAt } from "npm:remeda";
+import * as semver from "jsr:@std/semver";
 
 type McrDockerTagsResponse = {
-    name: string;
-    tags: string[];
+  name: string;
+  tags: string[];
 };
-
-type GhcrDockerTagsResponse = { name: string, metadata: { container: { tags: string[] } } }[];
 
 const config = {
-    GITHUB_TOKEN: Deno.env.get("GITHUB_TOKEN"),
-    GITHUB_OUTPUT: Deno.env.get("GITHUB_OUTPUT"),
-    ARCHITECTURE: Deno.env.get("ARCHITECTURE"),
-    org: "totto2727-org",
-    packageType: "container",
-    packageName: "playwright-ja",
+  GITHUB_OUTPUT: Deno.env.get("GITHUB_OUTPUT"),
+  LIMIT: Number.parseInt(Deno.env.get("LIMIT") ?? "") || 10,
+  IMAGE_NAME: Deno.env.get("IMAGE_NAME"),
 };
 
-if (!config.GITHUB_TOKEN || !config.GITHUB_OUTPUT || !config.ARCHITECTURE) {
-    throw new Error("GITHUB_TOKEN, GITHUB_OUTPUT, and ARCHITECTURE are not set");
+if (!config.GITHUB_OUTPUT || !config.IMAGE_NAME) {
+  throw new Error("GITHUB_OUTPUT and IMAGE_NAME are not set");
 }
 
-const GITHUB_TOKEN = config.GITHUB_TOKEN;
 const GITHUB_OUTPUT = config.GITHUB_OUTPUT;
-const ARCHITECTURE = config.ARCHITECTURE;
+const IMAGE_NAME = config.IMAGE_NAME;
+const LIMIT = config.LIMIT;
 
+function generateMcrUrl(imageName: string) {
+  return `https://mcr.microsoft.com/v2/${imageName}/tags/list`;
+}
+
+async function fetchMcr(
+  fetch: typeof globalThis.fetch,
+  req: Request,
+): Promise<McrDockerTagsResponse> {
+  const responseSource = await fetch(req);
+
+  if (!responseSource.ok || responseSource.status !== 200) {
+    throw new Error("Failed to fetch tags from MCR");
+  }
+
+  // TODO validate
+  return await responseSource.json();
+}
+
+function isArchitectureAmd64(tag: string): boolean {
+  return tag.includes("amd64");
+}
+
+function isArchitectureArm64(tag: string): boolean {
+  return tag.includes("arm64");
+}
+
+function addArm64TagSuffix(tag: string): string {
+  return `${tag}-arm64`;
+}
+
+function addAmd64TagSuffix(tag: string): string {
+  return `${tag}-amd64`;
+}
+
+function isBasicTag(tag: string): boolean {
+  return !tag.includes("-alpha") && !tag.includes("-beta") &&
+    !tag.includes("-next");
+}
+
+function isArchitectureMulti(
+  amd64: Set<string>,
+  arm64: Set<string>,
+  tag: string,
+): boolean {
+  return !isArchitectureAmd64(tag) && !isArchitectureArm64(tag) &&
+    amd64.has(addAmd64TagSuffix(tag)) && arm64.has(addArm64TagSuffix(tag));
+}
 
 async function main() {
+  const buildTargetResponse = await fetchMcr(
+    fetch,
+    new Request(generateMcrUrl(IMAGE_NAME)),
+  );
 
-    const responseSource = await fetch(
-        "https://mcr.microsoft.com/v2/playwright/tags/list",
-    );
-    if (!responseSource.ok || responseSource.status !== 200) {
-        throw new Error("Failed to fetch tags from MCR");
-    }
-    const jsonSource: McrDockerTagsResponse = await responseSource.json();
-    const filteredVersion = pipe(
-        jsonSource.tags,
-        filter((v) => v.startsWith("v")),
-        filter((v) => canParse(v) && greaterThan(parse(v), parse("1.40.0"))),
-        filter((v) => !v.includes("-alpha") && !v.includes("-beta")),
-        filter((v) => v.includes(ARCHITECTURE)),
-    );
-    filteredVersion.forEach(v => console.log("mcr", v))
+  const minSemver = pipe(
+    buildTargetResponse.tags,
+    filter(semver.canParse), // semverとしてパース可能なもののみ
+    map(semver.parse), // 元の文字列とsemverのタプルに変換
+    map((s) => `${s.major}.${s.minor}.${s.patch}`), // プリミティブなバージョンのみの文字列に変換
+    (v) => Array.from(new Set(v)), // 重複を削除
+    map(semver.parse), // 元の文字列とsemverのタプルに変換
+    sort((s1, s2) => semver.compare(s1, s2)), // ソート
+    reverse(), // 降順にする
+    (v) => last(splitAt(v, LIMIT)[0]), // 最初のLIMIT個までの最後の要素を取り出す
+  );
 
-    const responseTarget = await fetch(
-        `https://api.github.com/orgs/${config.org}/packages/${config.packageType}/${config.packageName}/versions`,
-        {
-            method: "GET",
-            headers: {
-                "Accept": "application/vnd.github+json",
-                "Authorization": `Bearer ${GITHUB_TOKEN}`,
-                "X-GitHub-Api-Version": "2022-11-28",
-            },
-        },
-    );
-    if (!responseSource.ok || responseSource.status !== 200) {
-        throw new Error("Failed to fetch tags from GHCR");
-    }
-    const jsonTarget: GhcrDockerTagsResponse = await responseTarget.json();
-    const setTarget = new Set(jsonTarget.flatMap((v) => [v.name, ...v.metadata.container.tags]));
-    Array.from(setTarget.values()).forEach(v => console.log("ghcp", v))
+  if (!minSemver) {
+    throw new Error("No valid semver found");
+  }
 
-    const requiredBuildTagList = { tag: filteredVersion.filter((v) => !setTarget.has(v)) };
-    requiredBuildTagList.tag.forEach(v => console.log("required", v))
+  console.log("minSemver", minSemver);
 
-    Deno.writeTextFileSync(
-        GITHUB_OUTPUT,
-        `matrix=${JSON.stringify(requiredBuildTagList)}\n`,
-        { append: true },
-    );
+  const targetBasicTagList = pipe(
+    buildTargetResponse.tags,
+    filter((tag) => tag.startsWith("v")), // vから始まるタグのみ
+    filter(isBasicTag), // 基本的なタグのみ
+    filter(semver.canParse), // semverとしてパース可能なもののみ
+    map((v) => [v, semver.parse(v)] as const), // 元の文字列とsemverのタプルに変換
+    filter(([_v, s]) => semver.greaterOrEqual(s, minSemver)), // 最低バージョン以上
+    sort(([_v1, s1], [_v2, s2]) => semver.compare(s1, s2)), // ソート
+    reverse(), // 降順にする
+    map(([v]) => v), // タプルの文字列を取り出す
+  );
+
+  const targetArm64 = pipe(
+    targetBasicTagList,
+    filter(isArchitectureArm64),
+  );
+  const targetArm64Set = new Set(targetArm64);
+  console.log("targetArm64", targetArm64.length, targetArm64);
+
+  const targetAmd64 = pipe(
+    targetBasicTagList,
+    filter(isArchitectureAmd64),
+  );
+  const targetAmd64Set = new Set(targetAmd64);
+  console.log("targetAmd64", targetAmd64.length, targetAmd64);
+
+  const targetMulti = pipe(
+    targetBasicTagList,
+    filter((v) => isArchitectureMulti(targetAmd64Set, targetArm64Set, v)),
+  );
+  console.log("targetMulti", targetMulti.length, targetMulti);
+
+  // 末尾改行
+  const output = `
+arm64=${JSON.stringify(targetArm64)}
+amd64=${JSON.stringify(targetAmd64)}
+multi=${JSON.stringify(targetMulti)}
+`.trimStart();
+
+  console.log(output);
+
+  Deno.writeTextFileSync(
+    GITHUB_OUTPUT,
+    output,
+    { append: true },
+  );
 }
 
 if (import.meta.main) {
-    await main();
+  await main();
 }
